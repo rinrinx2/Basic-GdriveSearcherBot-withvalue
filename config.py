dc6d66f0d3f723b7d4e263cb04cbc33ab1565e3e
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-06-26 13:11:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -9,47 +9,42 @@ DRIVE_NAME = [
     "2",  # folder 3 name
     "4",  # ....
     "5",  # ......
-    "6"  # and soo onnnn folder n names"1",  # folder 1 name
-
-    "7",  # folder 2 name
-
-    "8",  # folder 3 name
-
-    "9",  # ....
-
-    "10",  # ......
-
-    "11"  # and soo onnnn folder n names"1",  # folder 1 name
+    "6",  # and soo onnnn folder n name
+    "7",  # folder 2 nam
+    "8",  # folder 3 nam
+    "9",  # ...
+    "10",  # .....
+    "11"  # and soo onnnn folder n names
     "12",  # folder 2 name
     "13",  # folder 3 name
     "14",  # ....
     "15",  # ......
-    "16"  # and soo onnnn folder n names"1",  # folder 1 name
+    "16",  # and soo onnnn folder n names
     "17",  # folder 2 name
     "18",  # folder 3 name
     "19",  # ....
     "20",  # ......
-    "21"  # and soo onnnn folder n names"1",  # folder 1 name
+    "21",  # and soo onnnn folder n namesfolname
     "22",  # folder 2 name
     "23",  # folder 3 name
     "24",  # ....
     "25",  # ......
-    "26"  # and soo onnnn folder n nam"1",  # folder 1 name
+    "26",  # and soo onnnn folder n nam
     "27",  # folder 2 name
     "28",  # folder 3 name
     "29",  # ....
     "30",  # ......
-    "31"  # and soo onnnn folder n names"1",  # folder 1 name
+    "31",  # and soo onnnn folder n names
     "32",  # folder 2 name
     "33",  # folder 3 name
     "34",  # ....
     "35",  # ......
-    "36"  # and soo onnnn folder n names"1",  # folder 1 name
+    "36",  # and soo onnnn folder n names
     "37",  # folder 2 name
     "38",  # folder 3 name
     "39",  # ....
     "40",  # ......
-    "41"  # and soo onnnn folder n namesm
+    "41",  # and soo onnnn folder n namesm
 ]
 
 DRIVE_ID = [
@@ -59,125 +54,69 @@ DRIVE_ID = [
 "0AF6GSe5szgopUk9PVA",
 "0AO3RmaSwhxQ2Uk9PVA",
 "0AInmBU1dHhwJUk9PVA",
-    "1mmUK5AHB51u1GXw53i_0U_6VEf62AmHT",
-
+"1mmUK5AHB51u1GXw53i_0U_6VEf62AmHT",
 "1kQU-BlpAQe_lBsKBEQ1u9HSqZU2AN2xw",
-
 "0ACEM_XxjLaOZUk9PVA",
-
 "0AKhAHXxVeDP1Uk9PVA",
-
 "0AKFcTGWfx3U9Uk9PVA",
-
 "0AN4XtN1OGhoYUk9PVA",
-
 "0ADPlc-eHo4JcUk9PVA",
-
 "0AMIAQwRYOLXpUk9PVA",
-
 "0ALGSJKwIQHweUk9PVA",
-
 "0ALiNO3Hu006FUk9PVA",
-
 "0ADgIvgxFERrkUk9PVA",
-
 "0ADolfPBpwPHWUk9PVA",
-
 "0AIZwiPqjaAqFUk9PVA",
-
 "0APtl5rsd5FHQUk9PVA",
-
 "0APtl5rsd5FHQUk9PVA",
-
 "0AOM2i7MQiuWIUk9PVA",
-
 "0AL0LGwbBkeTuUk9PVA",
-
 "0AFmP8fiI9UEsUk9PVA",
-
 "0AMQP-8gNxWfaUk9PVA",
-
 "0AL6Tka2S7BPKUk9PVA",
-
 "0AB0U5rFDybQFUk9PVA",
-
 "0AEarpkDuiz9gUk9PVA",
-
 "0AEZqQMn_xaNRUk9PVA",
-
-
-
-
 ]
 
 INDEX_URL = [
-    "http://cdn.bdflix.ml/0:",
+
+"http://cdn.bdflix.ml/0:",
 "http://cdn.bdflix.ml/1:",
 "http://cdn.bdflix.ml/2:",
 "http://cdn.bdflix.ml/3:",
 "http://cdn.bdflix.ml/4:",
 "http://cdn.bdflix.ml/5:",
-
 "http://cdn.bdflix.ml/6:",
-
 "http://cdn.bdflix.ml/7:",
-
 "http://cdn.bdflix.ml/8:",
-
 "http://cdn.bdflix.ml/9:",
-
 "http://cdn.bdflix.ml/10:"
-
 "http://cdn.bdflix.ml/11:",
-
 "http://cdn.bdflix.ml/12:",
-
 "http://cdn.bdflix.ml/13:",
-
 "http://cdn.bdflix.ml/14:",
-
 "http://cdn.bdflix.ml/15:",
-
 "http://cdn.bdflix.ml/16:",
-
 "http://cdn.bdflix.ml/17:",
-
 "http://cdn.bdflix.ml/18:",
-
 "http://cdn.bdflix.ml/19:",
-
 "http://cdn.bdflix.ml/20:",
-
 "http://cdn.bdflix.ml/21:",
-
 "http://cdn.bdflix.ml/22:",
-
 "http://cdn.bdflix.ml/23:",
-
 "http://cdn.bdflix.ml/24:",
-
 "http://cdn.bdflix.ml/25:",
-
 "http://cdn.bdflix.ml/26:",
-
 "http://cdn.bdflix.ml/27:",
-
 "http://cdn.bdflix.ml/28:",
-
 "http://cdn.bdflix.ml/29:",
-
 "http://cdn.bdflix.ml/30:",
-
 "http://cdn.bdflix.ml/31:",
-
 "http://cdn.bdflix.ml/32:",
-
 "http://cdn.bdflix.ml/33:",
-
 "http://cdn.bdflix.ml/34:",
-
 "http://cdn.bdflix.ml/35:",
-
 "http://cdn.bdflix.ml/36:",
 "http://cdn.bdflix.ml/37:",
 ]
