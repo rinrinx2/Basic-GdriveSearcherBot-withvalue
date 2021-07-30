e76c2b5b8b5a79a9db2ae727ce6e371442a98d27
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-07-30 23:00:24 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,92 +4,93 @@ SUDO_CHATS_ID = [ 993876207, -575492766, -1001319419576, -1001190259319, -100135
 
 
 DRIVE_NAME = [
-	"Lost_In_the_Ocene",	# 0
+
+    "Lost_In_the_Ocene",	# 0
     "Bangla_Movies",		# 1
-	"Bollywood",  			# 2
-    "Hollywood",  			# 3
+    "Bollywood",  		# 2
+    "Hollywood",  		# 3
     "IMDB_Top_List",  		# 4
     "Collection_Pack",  	# 5    
     "South_Indian",  		# 6
-    "PSA_Movies",  			# 7
-    "PSA_Series",  			# 8
-    "TV_Series",  			# 9
-    "Courses",  			# 10
-    "DC_Zero",  			# 11
+    "PSA_Movies",  		# 7
+    "PSA_Series",  		# 8
+    "TV_Series",  		# 9
+    "Courses",  		# 10
+    "DC_Zero",  		# 11
     "Bot_Uploads_2",  		# 12
-    "APDS_1",  				# 13
-    "APDS_2",  				# 14
-    "APDS_1.0",  			# 15
-    "APDS_1.2",  			# 16
-    "APDS_1.3",  			# 17
-    "APDS_2.1",  			# 18
-    "APDS_2.2",  			# 19
-    "APDS_2.3",  			# 20
-    "APDS_2.4",  			# 21
-    "Xtreme_6",  			# 22
-	"GDMC_1",  				# 23
-    "GDMC_2",  				# 23
-    "GDMC_3",  				# 25
-    "DEC_20",  				# 26
-    "Jan_21",  				# 27
-    "Jan_21_1",  			# 28
-    "March_21",  			# 29
-    "June_21",  			# 30
-    "May_21",  				# 31
-	"Oct_20",  				# 32
-    "Sep_20",  				# 33
-	"Aug_20",  				# 34
-    "July_20",  			# 35
-	"March_20",  			# 36
-	"Xtreme_1",  			# 37
-    "Xtreme_2",  			# 38
-	"Xtreme_3",  			# 39
-    "Xtreme_4",  			# 40
+    "APDS_1",  			# 13
+    "APDS_2",  			# 14
+    "APDS_1.0",  		# 15
+    "APDS_1.2",  		# 16
+    "APDS_1.3",  		# 17
+    "APDS_2.1",  		# 18
+    "APDS_2.2",  		# 19
+    "APDS_2.3",  		# 20
+    "APDS_2.4",  		# 21
+    "Xtreme_6",  		# 22
+    "GDMC_1",  			# 23
+    "GDMC_2",  			# 23
+    "GDMC_3",  			# 25
+    "DEC_20",  			# 26
+    "Jan_21",  			# 27
+    "Jan_21_1",  		# 28
+    "March_21",  		# 29
+    "June_21",  		# 30
+    "May_21",  			# 31
+    "Oct_20",  			# 32
+    "Sep_20",  			# 33
+    "Aug_20",  			# 34
+    "July_20",  		# 35
+    "March_20",  		# 36
+    "Xtreme_1",  		# 37
+    "Xtreme_2",  		# 38
+    "Xtreme_3",  		# 39
+    "Xtreme_4",  		# 40
 ]
 
 DRIVE_ID = [
-    
-"0AJtq-dCXNVYwUk9PVA", 				 #Lost In the Ocene
-"1UlKMzEtvWUqmPFIgveo5Uy_jwV1qR9R_", #1
-"1VJkDLP3bv6InG9R_W9tBC45BxU3CH0fx", #2
-"1NSCH5JB6Bl4_zhn6yIuwRiiarvhtcHkz", #3
-"1eYjlGPCxXP1zUDFu3O9YaeZWnbzxT-p1", #4
-"1hAThVZuFOmG2B5PJ9nolMwr9MuM2ssN8", #5
-"1_kXJpnyXFnRpiejZtmrUCp1DOQs5C0bf", #6
-"19H_bD_MKRQ70qH6pCKS4z5969VdPzlaf", #7
-"131Cl1yDNOGW0ahkB07grHGATM7Nfb2lV", #8
-"1GhdZDc6iHisfDiUlyRkUDlWjHyeRNbk6", #9
-"1QRljHTOJgYdgQ1iUs3GAplZPXE5vJB60", #10
-"0ACxIdvo1MF53Uk9PVA", 				 #11
-"0AF6GSe5szgopUk9PVA", 				 #12
-"0AKhAHXxVeDP1Uk9PVA", 				 #13
-"0AKFcTGWfx3U9Uk9PVA", 				 #14
-"0AN4XtN1OGhoYUk9PVA", 				 #15
-"0ADPlc-eHo4JcUk9PVA", 				 #16
-"0AMIAQwRYOLXpUk9PVA", 				 #17
-"0ALGSJKwIQHweUk9PVA", 				 #18
-"0ALiNO3Hu006FUk9PVA", 				 #19
-"0ADgIvgxFERrkUk9PVA",   			 #20
-"0AO3RmaSwhxQ2Uk9PVA", 				 #21
-"136ktqB6IW4Rg6SDlgvFDqigfZZ1BUnJV", #22
-"13BDf37_7tBc680dQ6F6G7_5tP5I81471", #23
-"1bVo4iuXgQ2h5e2qfQQU2-BRCi0fjSAUc-", #24
-"1O-xmbS8QFIE0QY1AzMr3HYzPwvW5ZbYx", #25
-"1SE4US3pprtC7lbA1CZb-LwaFGEjgnay2", #26
-"18ngROC4tLF2uKGpo0VkE5Elp94MJMIk4", #27
-"1sEcpXGAbhT3X7kOE1g8knbvh5jOTUx7K", #28
-"1Au7Ed8ibC8tE0l3Tf4UNR55qM9ogkca9", #29
-"1-SOFqWFHpckALJz437-irVhAnjzxgLkW", #30
-"1SrbdybfP0gB8HNup2uPMYzvidT10o2qW", #31
-"1CxdVc9C-6-sllOe8lD1IqWy1uKbvKmBD", #32
-"1D5N5DddEoz1KCUGHjUYD3TeuqskTvbZj", #33
-"1guot-8-dGoY1tJ2UeroBi_QkDAC_wMDS", #34
-"1rMsivIt0M6BlZXgiOaLWmeyap8EX6YWE", #35
-"1iGNP47SiCy-NI9h755EdtAa4hK4aaaJ5", #36
-"164Zwctr57FGN3YmAPAHH99PueKhp7hL0", #37
-"1O1kTFP-T2itcxslRxgqOKa4RkZto_2jf", #38
-"1_wfopFhnP7um9a3234glOqQ6A5kUIdUE", #39
-"1DqvkdLqBAJZTMFt-isDj09PelD_9nM7W", #40
+
+"0AJtq-dCXNVYwUk9PVA", 
+"1UlKMzEtvWUqmPFIgveo5Uy_jwV1qR9R_",
+"1VJkDLP3bv6InG9R_W9tBC45BxU3CH0fx",
+"1NSCH5JB6Bl4_zhn6yIuwRiiarvhtcHkz",
+"1eYjlGPCxXP1zUDFu3O9YaeZWnbzxT-p1",
+"1hAThVZuFOmG2B5PJ9nolMwr9MuM2ssN8",
+"1_kXJpnyXFnRpiejZtmrUCp1DOQs5C0bf",
+"19H_bD_MKRQ70qH6pCKS4z5969VdPzlaf",
+"131Cl1yDNOGW0ahkB07grHGATM7Nfb2lV",
+"1GhdZDc6iHisfDiUlyRkUDlWjHyeRNbk6",
+"1QRljHTOJgYdgQ1iUs3GAplZPXE5vJB60",
+"0ACxIdvo1MF53Uk9PVA",
+"0AF6GSe5szgopUk9PVA",
+"0AKhAHXxVeDP1Uk9PVA",
+"0AKFcTGWfx3U9Uk9PVA",
+"0AN4XtN1OGhoYUk9PVA",
+"0ADPlc-eHo4JcUk9PVA",
+"0AMIAQwRYOLXpUk9PVA",
+"0ALGSJKwIQHweUk9PVA",
+"0ALiNO3Hu006FUk9PVA",
+"0ADgIvgxFERrkUk9PVA",
+"0AO3RmaSwhxQ2Uk9PVA",
+"136ktqB6IW4Rg6SDlgvFDqigfZZ1BUnJV",
+"13BDf37_7tBc680dQ6F6G7_5tP5I81471",
+"1bVo4iuXgQ2h5e2qfQQU2-BRCi0fjSAUc-",
+"1O-xmbS8QFIE0QY1AzMr3HYzPwvW5ZbYx",
+"1SE4US3pprtC7lbA1CZb-LwaFGEjgnay2",
+"18ngROC4tLF2uKGpo0VkE5Elp94MJMIk4",
+"1sEcpXGAbhT3X7kOE1g8knbvh5jOTUx7K",
+"1Au7Ed8ibC8tE0l3Tf4UNR55qM9ogkca9",
+"1-SOFqWFHpckALJz437-irVhAnjzxgLkW",
+"1SrbdybfP0gB8HNup2uPMYzvidT10o2qW",
+"1CxdVc9C-6-sllOe8lD1IqWy1uKbvKmBD",
+"1D5N5DddEoz1KCUGHjUYD3TeuqskTvbZj",
+"1guot-8-dGoY1tJ2UeroBi_QkDAC_wMDS",
+"1rMsivIt0M6BlZXgiOaLWmeyap8EX6YWE",
+"1iGNP47SiCy-NI9h755EdtAa4hK4aaaJ5",
+"164Zwctr57FGN3YmAPAHH99PueKhp7hL0",
+"1O1kTFP-T2itcxslRxgqOKa4RkZto_2jf",
+"1_wfopFhnP7um9a3234glOqQ6A5kUIdUE",
+"1DqvkdLqBAJZTMFt-isDj09PelD_9nM7W",
 ]
 
 INDEX_URL = [
