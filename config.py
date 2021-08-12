92877ec938f95fec04884e8dcb8c2528739f5091
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-08-13 04:08:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ DRIVE_NAME = [
     "Bangla_Movies",		# 1
     "Bollywood",  		# 2
     "Hollywood",  		# 3
-    "IMDB_Top_List",  	# 4
-    "Collection_Pack",  # 5    
-    "South_Indian",  	# 6
+    "IMDB_Top_List",  		# 4
+    "Collection_Pack",  	# 5    
+    "South_Indian",  		# 6
     "PSA_Movies",  		# 7
     "PSA_Series",  		# 8
     "TV_Series",  		# 9
     "Courses",  		# 10
     "DC_Zero",  		# 11
-    "Bot_Uploads_2",  	# 12
+    "Bot_Uploads_2",  		# 12
     "APDS_1",  			# 13
     "APDS_2",  			# 14
     "APDS_1.0",  		# 15
@@ -46,6 +46,46 @@ DRIVE_NAME = [
     "Xtreme_5",  		# 38
     "Shinobi_1",  		# 39
     "Shinobi_2",  		# 40
+    "DCone",  		# 1
+    "DCtwo",  		# 2
+    "pcgames",  	# 3  
+    "gdm1",  		# 4
+    "gdm2",  		# 5
+    "gdm4",  		# 6
+    "gdm5",  		# 7
+    "gdm6",  		# 8
+    "blz1",  		# 9
+    "blz2",  		# 10
+    "blz3",  		# 11
+    "blz4",  		# 12
+    "Abrar",  		# 13
+    "mhj",  		# 14
+    "mhjP",  		# 15
+    "mhTD1",  		# 16
+    "mhTD2",  		# 17
+    "mhTD3",  		# 18
+    "mhTD4",  		# 19
+    "mhTD5",  		# 20
+    "mhTD6",  		# 21
+    "mhTD7",  		# 22
+    "mhTD7",  		# 23
+    "ausgt21",  	# 24
+    "slbots",  		# 25
+    "slold_f1",  		# 26
+    "slold_f2",  		# 27
+    "slold_f3",  		# 28
+    "slcontent_1080p",  		# 29
+    "slcontent_hindi_90s",  		# 30
+    "slcontent_hindi_2000",  		# 31
+    "slcontens_hindi_10-14",  		# 32
+    "slcon_hindi_15-17",  		# 33
+    "slcon_hindi_5",  		# 34
+    "Hindi_1080p",  		# 35
+    "",  		# 36
+    "",  		# 37
+    "",  		# 38
+    "",  		# 39
+    "",  		# 40
 ]
 
 DRIVE_ID = [
@@ -91,6 +131,50 @@ DRIVE_ID = [
 "1grD_3UjRv2OtRPXgGnsVDzlpWQgcBdZx",
 "1mmUK5AHB51u1GXw53i_0U_6VEf62AmHT",
 "1kQU-BlpAQe_lBsKBEQ1u9HSqZU2AN2xw",
+"0AOmCfZYnzSxPUk9PVA",
+"0ACKT35lvMcGnUk9PVA",
+"0AFXUwlshWnZtUk9PVA",
+"1cXxOtuR8xgsMppA1MjTsUSUktDKXQfqi",
+"1fZp98s7iDRwWeW0S4Q7CQXVZbrrOGNjI",
+"1zXldes4Q0CeJXTR18lZvY-4kTDgIvtHg",
+"1yZ4ir37Krt5Pt0ZpaCjZ1TV5EYqmP_W6",
+"1oy4zgaGt7GDMHMKzsRl37npatV48rJTu",
+"1Ghz-NT_mvvlGBdDX2hrafetdAWU95gAb",
+"1TFArAACVAjYz2daSBBc9jsFNbH3G16x4",
+"1YlMRBZbEv4wXBbUYcyva5Qpn3ds6m0La",
+"1027qKuZGLjBh3_mFBQN4ejz6eEmxeck6",
+"1JPMlwGH8JHWEryjCFNeHOKxyDNF6E7Ug",
+"0AGRh7-6BtgzmUk9PVA",
+"0AC1woHItpMG_Uk9PVA",
+"1-PWrAunH92rxLKnPbBSRrMEASMZ3jqbg",
+"0ADkoqVE06_tiUk9PVA",
+"0AGyxparFnWEmUk9PVA",
+"0AEjlASCBDyCQUk9PVA",
+"0ALZf6od8OUvYUk9PVA",
+"0AJ-T74bez-XLUk9PVA",
+"0ANzSPxSpWWN0Uk9PVA",
+"0AHR2Pgqjg8CtUk9PVA",
+"1d_nMzq_N19GFIwZn-lHjI001G8pesmb9",
+"1fhbUFp9gDpyEsM47CPfS9SuyI9PrLh8c",
+"1AfmXmhPnQ08xhIQq6JJ4WqXadoQ4kRIz",
+"1A5W-pFBd9z5KDZMUBM0Y7FLPMRzabPVS",
+"1RQZ58wka72LT_5v2IlEdwSpze-wEEyHl",
+"1E8fwqoHmAe4LGbT5n_MK9AV3Y5OQNQub",
+"1ZK1UnQF7ix06mYqGnRORVNYlJ7-2zZfD",
+"1ZUCtEc2z5ZhOAsHHnstT1iYS35iFZJDh",
+"1rHPcrPd3pQnFVf1eqRDSQgm08PKC13zg",
+"1GHzj9fsxnW9mYdXZ9dA8oRhY0fb0PAL7",
+"1TbmPQD_ffLcQ24tJ3yuxD4RgP50Se2Rk",
+"1ZcuWM29L6Gb2pLH5cMIY-9mruNPBU2IM",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
 ]
 
 INDEX_URL = [
@@ -136,4 +220,44 @@ INDEX_URL = [
 "http://search.indexbd.cf/38:",
 "http://search.indexbd.cf/39:",
 "http://search.indexbd.cf/40:",
+"http://search2.indexbd.cf/1:",
+"http://search2.indexbd.cf/2:",
+"http://search2.indexbd.cf/3:",
+"http://search2.indexbd.cf/4:",
+"http://search2.indexbd.cf/5:",
+"http://search2.indexbd.cf/6:",
+"http://search2.indexbd.cf/7:",
+"http://search2.indexbd.cf/8:",
+"http://search2.indexbd.cf/9:",
+"http://search2.indexbd.cf/10:",
+"http://search2.indexbd.cf/11:",
+"http://search2.indexbd.cf/12:",
+"http://search2.indexbd.cf/13:",
+"http://search2.indexbd.cf/14:",
+"http://search2.indexbd.cf/15:",
+"http://search2.indexbd.cf/16:",
+"http://search2.indexbd.cf/17:",
+"http://search2.indexbd.cf/18:",
+"http://search2.indexbd.cf/19:",
+"http://search2.indexbd.cf/20:",
+"http://search2.indexbd.cf/21:",
+"http://search2.indexbd.cf/22:",
+"http://search2.indexbd.cf/23:",
+"http://search2.indexbd.cf/24:",
+"http://search2.indexbd.cf/25:",
+"http://search2.indexbd.cf/26:",
+"http://search2.indexbd.cf/27:",
+"http://search2.indexbd.cf/28:",
+"http://search2.indexbd.cf/29:",
+"http://search2.indexbd.cf/30:",
+"http://search2.indexbd.cf/31:",
+"http://search2.indexbd.cf/32:",
+"http://search2.indexbd.cf/33:",
+"http://search2.indexbd.cf/34:",
+"http://search2.indexbd.cf/35:",
+"http://search2.indexbd.cf/36:",
+"http://search2.indexbd.cf/37:",
+"http://search2.indexbd.cf/38:",
+"http://search2.indexbd.cf/39:",
+"http://search2.indexbd.cf/40:",
 ]
