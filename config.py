db334e35292f32cdfc9815b63f1800476e6877b1
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2022-01-28 04:27:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "1884423253:AAGiVT3v_6ghMFNwWaKsdQW95TWfPeABI-4"
-RESULTS_COUNT = 4  # NOTE Nuber of results to show, 4 is better
-SUDO_CHATS_ID = [ 993876207, -575492766, -1001319419576, -1001190259319, -1001352599350, 1304152521, 993876207, -497415557]
+BOT_TOKEN = "5101638032:AAGW0XinkptuCNFziqP3OH-sWcLMD4iIU38"
+RESULTS_COUNT = 10  # NOTE Nuber of results to show, 4 is better
+SUDO_CHATS_ID = [ 993876207, -575492766, -1001319419576, -1001190259319, -1001352599350, 1304152521, 993876207, -497415557, -1001620235788, 1224542559, 1857894147, 1881785806]
 SUDO_CHATS_ID_GS = [ 993876207, 1304152521 ]
 
 DRIVE_NAME = [
@@ -11,316 +11,48 @@ DRIVE_NAME = [
     "Hollywood",  		# 3
     "IMDB_Top_List",  		# 4
     "Collection_Pack",  	# 5    
-    "South_Indian",  		# 6
-    "PSA_Movies",  		# 7
-    "PSA_Series",  		# 8
-    "TV_Series",  		# 9
-    "Courses",  		# 10
-    "DC_Zero",  		# 11
-    "Bot_Uploads_2",    # 12
-    "APDS_1",  			# 13
-    "APDS_2",  			# 14
-    "APDS_1.0",  		# 15
-    "APDS_1.2",  		# 16
-    "APDS_1.3",  		# 17
-    "APDS_2.1",  		# 18
-    "APDS_2.2",  		# 19
-    "APDS_2.3",  		# 20
-    "APDS_2.4",  		# 21
-    "Xtreme_6",  		# 22   
-    "DEC_20",  			# 23
-    "Jan_21",  			# 24
-    "Jan_21_1",  		# 25
-    "March_21",  		# 26
-    "June_21",  		# 27
-    "May_21",  			# 28
-    "Oct_20",  			# 29
-    "Sep_20",  			# 30
-    "Aug_20",  			# 31
-    "July_20",  		# 32
-    "March_20",  		# 33
-    "Xtreme_1",  		# 34
-    "Xtreme_2",  		# 35  
-    "Xtreme_3",  		# 36
-    "Xtreme_4",  		# 37
-    "Xtreme_5",  		# 38
-    "Shinobi_1",  		# 39
-    "Shinobi_2",  		# 40
-    "RSSMO1080",  			# 1
-    "RSSmo720",  			# 2
-    "pcgames",  		# 3  
-    "gdm1",  			# 4
-    "gdm2",  			# 5
-    "gdm4",  			# 6
-    "gdm5",  			# 7
-    "gdm6",  			# 8
-    "blz1",  			# 9
-    "blz2",  			# 10
-    "blz3",  			# 11
-    "blz4",  			# 12
-    "Abrar",  			# 13
-    "mhj",  			# 14
-    "mhjP",  			# 15
-    "mhTD1",  			# 16
-    "mhTD2",  			# 17
-    "mhTD3",  			# 18
-    "mhTD4",  			# 19
-    "mhTD5",  			# 20
-    "mhTD6",  			# 21
-    "mhTD7",  			# 22
-    "mhTD7",  			# 23
-    "ausgt21",  		# 24
-    "slbots",  			# 25
-    "slold_f1",  		# 26
-    "slold_f2",  		# 27
-    "slold_f3",  		# 28
-    "slcontent_1080p",  	# 29
-    "slcontent_hindi_90s",  	# 30
-    "slcontent_hindi_2000",  	# 31
-    "slcontens_hindi_10-14",  	# 32
-    "slcon_hindi_15-17",  	# 33
-    "slcon_hindi_5",  		# 34
-    "Hindi_1080p",  		# 35    
 ]
 
 DRIVE_ID = [
 
-"0AJtq-dCXNVYwUk9PVA",
-"1UlKMzEtvWUqmPFIgveo5Uy_jwV1qR9R_",
-"1VJkDLP3bv6InG9R_W9tBC45BxU3CH0fx",
-"1NSCH5JB6Bl4_zhn6yIuwRiiarvhtcHkz",
-"1eYjlGPCxXP1zUDFu3O9YaeZWnbzxT-p1",
-"1hAThVZuFOmG2B5PJ9nolMwr9MuM2ssN8",
-"1_kXJpnyXFnRpiejZtmrUCp1DOQs5C0bf",
-"19H_bD_MKRQ70qH6pCKS4z5969VdPzlaf",
-"131Cl1yDNOGW0ahkB07grHGATM7Nfb2lV",
-"1GhdZDc6iHisfDiUlyRkUDlWjHyeRNbk6",
-"1QRljHTOJgYdgQ1iUs3GAplZPXE5vJB60",
-"0ACxIdvo1MF53Uk9PVA",
-"0AF6GSe5szgopUk9PVA",
-"0AKhAHXxVeDP1Uk9PVA",
-"0AKFcTGWfx3U9Uk9PVA",
-"0AN4XtN1OGhoYUk9PVA",
-"0ADPlc-eHo4JcUk9PVA",
-"0AMIAQwRYOLXpUk9PVA",
-"0ALGSJKwIQHweUk9PVA",
-"0ALiNO3Hu006FUk9PVA",
-"0ADgIvgxFERrkUk9PVA",
-"0AO3RmaSwhxQ2Uk9PVA",
-"1O-xmbS8QFIE0QY1AzMr3HYzPwvW5ZbYx",
-"1SE4US3pprtC7lbA1CZb-LwaFGEjgnay2",
-"18ngROC4tLF2uKGpo0VkE5Elp94MJMIk4",
-"1sEcpXGAbhT3X7kOE1g8knbvh5jOTUx7K",
-"1Au7Ed8ibC8tE0l3Tf4UNR55qM9ogkca9",
-"1-SOFqWFHpckALJz437-irVhAnjzxgLkW",
-"1SrbdybfP0gB8HNup2uPMYzvidT10o2qW",
-"1CxdVc9C-6-sllOe8lD1IqWy1uKbvKmBD",
-"1D5N5DddEoz1KCUGHjUYD3TeuqskTvbZj",
-"1guot-8-dGoY1tJ2UeroBi_QkDAC_wMDS",
-"1rMsivIt0M6BlZXgiOaLWmeyap8EX6YWE",
-"1iGNP47SiCy-NI9h755EdtAa4hK4aaaJ5",
-"164Zwctr57FGN3YmAPAHH99PueKhp7hL0",
-"1O1kTFP-T2itcxslRxgqOKa4RkZto_2jf",
-"1_wfopFhnP7um9a3234glOqQ6A5kUIdUE",
-"1DqvkdLqBAJZTMFt-isDj09PelD_9nM7W",
-"1grD_3UjRv2OtRPXgGnsVDzlpWQgcBdZx",
-"1mmUK5AHB51u1GXw53i_0U_6VEf62AmHT",
-"1kQU-BlpAQe_lBsKBEQ1u9HSqZU2AN2xw",
-"1VCPCYGRoYPUEO7lWHukcMgHSyLD3GaT5",
-"1-bVjEwoXmnG-C2bm1nQQZMtT4jO72aij",
-"1C4HF43XNbgEbebN-ym0dnIwHOV_gDu8b",
-"1cXxOtuR8xgsMppA1MjTsUSUktDKXQfqi",
-"1fZp98s7iDRwWeW0S4Q7CQXVZbrrOGNjI",
-"1zXldes4Q0CeJXTR18lZvY-4kTDgIvtHg",
-"1yZ4ir37Krt5Pt0ZpaCjZ1TV5EYqmP_W6",
-"1oy4zgaGt7GDMHMKzsRl37npatV48rJTu",
-"1Ghz-NT_mvvlGBdDX2hrafetdAWU95gAb",
-"1TFArAACVAjYz2daSBBc9jsFNbH3G16x4",
-"1YlMRBZbEv4wXBbUYcyva5Qpn3ds6m0La",
-"1027qKuZGLjBh3_mFBQN4ejz6eEmxeck6",
-"1JPMlwGH8JHWEryjCFNeHOKxyDNF6E7Ug",
-"0AGRh7-6BtgzmUk9PVA",
-"0AC1woHItpMG_Uk9PVA",
-"1-PWrAunH92rxLKnPbBSRrMEASMZ3jqbg",
-"0ADkoqVE06_tiUk9PVA",
-"0AGyxparFnWEmUk9PVA",
-"0AEjlASCBDyCQUk9PVA",
-"0ALZf6od8OUvYUk9PVA",
-"0AJ-T74bez-XLUk9PVA",
-"0ANzSPxSpWWN0Uk9PVA",
-"0AHR2Pgqjg8CtUk9PVA",
-"1d_nMzq_N19GFIwZn-lHjI001G8pesmb9",
-"1fhbUFp9gDpyEsM47CPfS9SuyI9PrLh8c",
-"1AfmXmhPnQ08xhIQq6JJ4WqXadoQ4kRIz",
-"1A5W-pFBd9z5KDZMUBM0Y7FLPMRzabPVS",
-"1RQZ58wka72LT_5v2IlEdwSpze-wEEyHl",
-"1E8fwqoHmAe4LGbT5n_MK9AV3Y5OQNQub",
-"1ZK1UnQF7ix06mYqGnRORVNYlJ7-2zZfD",
-"1ZUCtEc2z5ZhOAsHHnstT1iYS35iFZJDh",
-"1rHPcrPd3pQnFVf1eqRDSQgm08PKC13zg",
-"1GHzj9fsxnW9mYdXZ9dA8oRhY0fb0PAL7",
-"1TbmPQD_ffLcQ24tJ3yuxD4RgP50Se2Rk",
-"1ZcuWM29L6Gb2pLH5cMIY-9mruNPBU2IM",
-"1PGEUJApE-ORfxyQfcio3RaIVHfFvAORI",
-"1WRZjg9Z4ls2EWOWomDdpMC7NkwmPFmLZ",
-"1tat-X_7oS8SlA45uX6hrcm5gyS9MbnmK",
-"1VMu38EPKM1XBfzK7u2wn5nwuc2cJMvEf",
-"1vJmoqJi0SqUEAqluKAQ4u5z1y-LKIIJu",
-"1udZ58YOrRU-E2bbsgSrjpeSrEVTfQQBu",
-"1n-XXOw4FADHZLX3q9B8xBNud64PoNugE",
-"10bSAOuwjZUEfep0hYP5ZIJwyhAab9ApL",
-"1RPYqFN-qwrXqZjRY07ahpGLhg5-nSQN6",
-"1HhxDjKidxgdOfx9WMckOkKUkDixxGAoQ",
-"19eITtEIH0dMLlZ67Eb8Wc_zI2yWVx7Co",
-"1lkyAUBAYcxdOxHr7fAsv3T-D6JAv60W4",
-"1dryi92O0xv9g2GH57_-lV7xPPIASR0I2",
-"1bFzSXdvKwTYNsqssAxsIP9KjAyNyHNn1",
-"1NvnN0QfeYP096Hv6Ly_iCBMlln1Kp0om",
-"1_rFKpzwgZL3iJgV5gcxBpTqQnx7h9eEs",
-"16czVI9ZwNUQRIio1XOWrxxGyoGCg4g5M",
-"1f9zhCcAetn7TuUlB8z8e-lh_GtnJu_zn",
-"1fD9TrkOdq6SboKe9f2GXJTK4sQDcwb7t",
-"1rOzV8l7pRJE_NnQ97pIP1GtyAWwvfZAE",
-"1LG4InWLqXGqKscDgZK83J94hg0WH7Uqo",
-"1YHOTmXEBH3Q912qwswscI-hNSBpkXlmi",
-"1AjcQZqCgLZXEX_XDOQi6HbSjpaa4sNco",
-"1dCvd0gePZV_B3kfr2kK2XuigD30cmXLh",
-"1GujcqvJVX4A7WQ_akLkCWHQSIUy_yEFS",
-"1B6A8T8s8R-hXEaNO_0v59GeAvVBnCGTT",
-"1HaI4zz_qDLgvLlO5UL3M3sZq0J8qox_j",
-"1StegCht11U_dZzQdthz3iysQi8Sj8dZw",
-"1sUPKffhIwcm0K69htrhi0WuoPZkEhDBi",
-"1t_rzu4I-aAu6ySkWzCyYpZ9vKJEEjiA0",
-"1kb-j_doUidBuO496dGArkJ2J4NC3AglE",
-"19OUCSLvVL9Fc_d5LhNhvKBkTNRxTjFdP",
-"1DVd19-RtHe4f89OlN_iq9rLEG0U4Vufc",
-"1Ac7LCB0RLLXXMxHIYF1GVRxuymdjXbaa",
-"1Zvmac5W_t1S2bnRY01yjzUmdaSE90TuH",
-"1ilWTJpwO6Txt_7XGACDYCk2CNKT2FgEK",
-"1IsdQXGAPLhPfaoGvdYN8TRvdMBbepPnP",
-"1tJtdfK79kOsLGpPITfuklriNUkTghq6c",
-"1G3HsKLawDT4XeC-SwGY3O1hIwaDcdlM9",
-"189k827BYKOCJlLnrgech6v0kuuBRlTmn",
-"1nFQmpnlAaqZdqRRjZ5iSzWTauZDS-guz",
+"1n_Onxl3w5wSq2AxNvDZEQA6PrFFklTnx",  # https://drive.google.com/drive/u/0/folders/1n_Onxl3w5wSq2AxNvDZEQA6PrFFklTnx ✅
+"1GkPs8V74VOY9HfdKKEIYZXW_bLLSKXbq",  # https://drive.google.com/drive/u/0/folders/1GkPs8V74VOY9HfdKKEIYZXW_bLLSKXbq ✅
+"1FC-pveZBLJfiGHtBgbmv3ZzuAvTn2OJ6",  # https://drive.google.com/drive/u/0/folders/1FC-pveZBLJfiGHtBgbmv3ZzuAvTn2OJ6 ✅
+"1CSUIK4szNC2NUC5YUSwgupC6KkChPLgQ",  # https://drive.google.com/drive/u/0/folders/1CSUIK4szNC2NUC5YUSwgupC6KkChPLgQ ✅
+"1x5BVhSC8XX1Ucb0x5xpi4pcrEx0i82bU",  # https://drive.google.com/drive/u/0/folders/1x5BVhSC8XX1Ucb0x5xpi4pcrEx0i82bU ✅
+"1V-C4oeeT01lCrr0LbYBRi3XeV5d2wRPl",  # https://drive.google.com/drive/u/0/folders/1V-C4oeeT01lCrr0LbYBRi3XeV5d2wRPl ✅
+"1DccyyqCRW4yeLJDg9hDbQdR6sgGstICy",  #  ✅
+"1p40mlse-QgDCavY2RMROkydLI-K5iT8J",  # https://drive.google.com/drive/u/0/folders/1p40mlse-QgDCavY2RMROkydLI-K5iT8J ✅
+"1Xny9ZhwYWLQ-RqUg88Jt5rgYZ46C18ff",  # https://drive.google.com/drive/u/0/folders/1Xny9ZhwYWLQ-RqUg88Jt5rgYZ46C18ff ✅
+"1353xAw0G4dCoHX5FUYD8JBma752ORMrH",  # https://drive.google.com/drive/u/0/folders/1353xAw0G4dCoHX5FUYD8JBma752ORMrH ✅
+"0AKZ2teG0cYiyUk9PVA",  # https://drive.google.com/drive/u/0/folders/0AKZ2teG0cYiyUk9PVA ✅
+"0AF6GSe5szgopUk9PVA",  #  ✅
+"1Lg6wgYD1fcW0CIuxzeT2p9oaeSl-B1_G",  # https://drive.google.com/drive/u/0/folders/1Lg6wgYD1fcW0CIuxzeT2p9oaeSl-B1_G ✅
+"1-bVjEwoXmnG-C2bm1nQQZMtT4jO72aij",  # https://drive.google.com/drive/u/0/folders/1-bVjEwoXmnG-C2bm1nQQZMtT4jO72aij ✅
+"1VCPCYGRoYPUEO7lWHukcMgHSyLD3GaT5",  # https://drive.google.com/drive/u/0/folders/1VCPCYGRoYPUEO7lWHukcMgHSyLD3GaT5 ✅
+"1nFQmpnlAaqZdqRRjZ5iSzWTauZDS-guz",  # https://drive.google.com/drive/u/0/folders/1nFQmpnlAaqZdqRRjZ5iSzWTauZDS-guz ✅
+"0ACxIdvo1MF53Uk9PVA",  # https://drive.google.com/drive/u/0/folders/0ACxIdvo1MF53Uk9PVA ✅
+"1J9Bwqs4ZV6rID9S6l58IASOav3XX00LJ",  # https://drive.google.com/drive/u/0/folders/0ACxIdvo1MF53Uk9PVA ✅
 ]
 
 INDEX_URL = [
 
-"http://search.indexbd.cf/0:",
-"http://search.indexbd.cf/1:",
-"http://search.indexbd.cf/2:",
-"http://search.indexbd.cf/3:",
-"http://search.indexbd.cf/4:",
-"http://search.indexbd.cf/5:",
-"http://search.indexbd.cf/6:",
-"http://search.indexbd.cf/7:",
-"http://search.indexbd.cf/8:",
-"http://search.indexbd.cf/9:",
-"http://search.indexbd.cf/10:",
-"http://search.indexbd.cf/11:",
-"http://search.indexbd.cf/12:",
-"http://search.indexbd.cf/13:",
-"http://search.indexbd.cf/14:",
-"http://search.indexbd.cf/15:",
-"http://search.indexbd.cf/16:",
-"http://search.indexbd.cf/17:",
-"http://search.indexbd.cf/18:",
-"http://search.indexbd.cf/19:",
-"http://search.indexbd.cf/20:",
-"http://search.indexbd.cf/21:",
-"http://search.indexbd.cf/22:",
-"http://search.indexbd.cf/23:",
-"http://search.indexbd.cf/24:",
-"http://search.indexbd.cf/25:",
-"http://search.indexbd.cf/26:",
-"http://search.indexbd.cf/27:",
-"http://search.indexbd.cf/28:",
-"http://search.indexbd.cf/29:",
-"http://search.indexbd.cf/30:",
-"http://search.indexbd.cf/31:",
-"http://search.indexbd.cf/32:",
-"http://search.indexbd.cf/33:",
-"http://search.indexbd.cf/34:",
-"http://search.indexbd.cf/35:",
-"http://search.indexbd.cf/36:",
-"http://search.indexbd.cf/37:",
-"http://search.indexbd.cf/38:",
-"https://dontabuse:shinobicloud@one.shinobi.workers.dev/0:/Bot_1",
-"https://dontabuse:shinobicloud@two.shinobi.workers.dev/0:/Bot_2",
-"http://search2.indexbd.cf/1:",
-"http://search2.indexbd.cf/2:",
-"http://search2.indexbd.cf/3:",
-"http://search2.indexbd.cf/4:",
-"http://search2.indexbd.cf/5:",
-"http://search2.indexbd.cf/6:",
-"http://search2.indexbd.cf/7:",
-"http://search2.indexbd.cf/8:",
-"http://search2.indexbd.cf/9:",
-"http://search2.indexbd.cf/10:",
-"http://search2.indexbd.cf/11:",
-"http://search2.indexbd.cf/12:",
-"http://search2.indexbd.cf/13:",
-"http://search2.indexbd.cf/14:",
-"http://search2.indexbd.cf/15:",
-"http://search2.indexbd.cf/16:",
-"http://search2.indexbd.cf/17:",
-"http://search2.indexbd.cf/18:",
-"http://search2.indexbd.cf/19:",
-"http://search2.indexbd.cf/20:",
-"http://search2.indexbd.cf/21:",
-"http://search2.indexbd.cf/22:",
-"http://search2.indexbd.cf/23:",
-"http://search2.indexbd.cf/24:",
-"http://search2.indexbd.cf/25:",
-"http://search2.indexbd.cf/26:",
-"http://search2.indexbd.cf/27:",
-"http://search2.indexbd.cf/28:",
-"http://search2.indexbd.cf/29:",
-"http://search2.indexbd.cf/30:",
-"http://search2.indexbd.cf/31:",
-"http://search2.indexbd.cf/32:",
-"http://search2.indexbd.cf/33:",
-"http://search2.indexbd.cf/34:",
-"http://search2.indexbd.cf/35:",
-"https://s2.packsindex.workers.dev/0:/10bit%20Collection",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/10bit",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/3xO",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/afm72",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/ArcX",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Balthallion",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/bandi",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/d3g",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/DUHiT",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Frys",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Goki",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/HxD",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/joy",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/kappa",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Natty%20(QXR)",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/prof",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/qman",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/r0b0t",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/r00t",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Ranvijay",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/sampa",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/TombDoc",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Vyndros",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Weasly%20HONE",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/Weasly%20HONE%202",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/XXXPAV69%20",
-"https://s2.packsindex.workers.dev/0:/30+%20Encoders%20Collection%20[50%20TB]/YOGI",
-"https://s2.packsindex.workers.dev/0:/3000+%20Movies%20Random%20Collection",
-"https://s2.packsindex.workers.dev/0:/Documentary%20Movies%20Collection",
-"https://s2.packsindex.workers.dev/0:/Hevcbay%20Collection",
-"https://s2.packsindex.workers.dev/0:/JYK%20Encodes%20Collection/JYK.720p.BRRip",
-"https://s2.packsindex.workers.dev/0:/JYK%20Encodes%20Collection/JYK.1080p.BluRay%20(A-Z)/JYK.1080p.BluRay.A-L%20[691GB]",
-"https://s2.packsindex.workers.dev/0:/JYK%20Encodes%20Collection/JYK.1080p.BluRay%20(A-Z)/JYK.1080p.BluRay.M-Z%20[747GB]",
-"https://s2.packsindex.workers.dev/0:/ShiNobi/MOVIES",
-"https://s2.packsindex.workers.dev/0:/ShiNobi/ShiNobi",
-"https://s2.packsindex.workers.dev/0:/ShiNobi/TV%20SERIES",
-"https://s2.packsindex.workers.dev/0:/Tigole%20Complete%20Collection/tigole%20-%20unsorted",
-"https://s2.packsindex.workers.dev/0:/Tollywood%20Collection/Tollywood%20Movies",
-"https://s2.packsindex.workers.dev/0:/TombDoc%20Collection",
-"https://s2.packsindex.workers.dev/0:/TombDoc%20Collection/!Series",
-"http://search2.indexbd.cf/0:",
+"https://cdn.indexbd.xyz/0:/Movies",
+"https://cdn.indexbd.xyz/0:/Movies/BOLLYWOOD",
+"https://cdn.indexbd.xyz/0:/Movies/HOLLYWOOD",
+"https://cdn.indexbd.xyz/0:/Movies/IMDB%20Top%20Listed",
+"https://cdn.indexbd.xyz/0:/Movies/PSA_Collectios/PSA%20Movies",
+"https://cdn.indexbd.xyz/0:/Movies/PSA_Collectios/PSA%20Series",
+"https://cdn.indexbd.xyz/0:/TV%20Series",
+"https://cdn.indexbd.xyz/1:/Korean%20Drama's/!%20Episode%20Wise",
+"https://cdn.indexbd.xyz/1:/Korean%20Drama's/!%20Ziped",
+"https://cdn.indexbd.xyz/1:/Korean%20Drama's/!%20Ongoing",
+"https://cdn.indexbd.xyz/2:",
+"https://cdn.indexbd.xyz/3:",
+"https://cdn.indexbd.xyz/3:/Chorki%20Web-DL-BDH",
+"https://cdn.indexbd.xyz/3:/Movie%20RSS%20720p/",
+"https://cdn.indexbd.xyz/3:/Movie%20RSS%201080p/",
+"https://cdn.indexbd.xyz/3:/TV%20Series%20RSS",
+"https://cdn.indexbd.xyz/4:",
+"https://cdn.indexbd.xyz/1:/Korean%20Movies",
 ]
